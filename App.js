12783e7967ae736a8c4adc0de7283c49b894b36a
Bhavya <bhavyadthakar01@gmail.com>
2022-05-12 09:20:03 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import AddTask from './src/screens/AddTasks/AddTasks';
+import AddUser from './src/screens/AddUser';
+import TodoList from './src/screens/TodoList/TodoList';
+import UserList from './src/screens/UserList/UserList';
+
+
+
+const App = () => {
+  return (
+    <AddUser />
+  );
+};
+
+
+export default App;
